56ae0d140d06858912cba2da90725df21bf84c5e
rocket1925 <81896901+rocket1925@users.noreply.github.com>
2021-07-03 21:40:01 +0530

    Add files via upload

---
+++ paper class.js
@@ -0,0 +1,25 @@
+class paper
+{
+    constructor(x,y,r)
+}
+var options={
+isStatic:false,
+restitution:0.3,
+friction:0,
+density:1.2
+
+   
+,thisimage=loadImage("paper.png"),
+this:x=x
+,this:y=y
+,this:image=loadImage("sprites/paper.png"),
+World,add(world,thisbody);
+}
+ display();
+ push();
+translate(this.body.position.x,this.body.position.y);
+rotate(angle);
+imageMode(CENTER);
+pop();
+
+ 
